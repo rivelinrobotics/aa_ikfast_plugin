881e7590b5a4351ead9dbebb006addba37494191
rr-robert <robert@rivelinrobotics.com>
2022-11-01 17:10:19 +0000

    added gp4 and gp4_toolchange ikfast plugins

---
+++ gp4_toolchange_ikfast_plugin/update_ikfast_plugin.sh
@@ -0,0 +1,22 @@
+search_mode=OPTIMIZE_MAX_JOINT
+srdf_filename=yaskawa.srdf
+robot_name_in_srdf=yaskawa
+moveit_config_pkg=yaskawa_moveit_config
+robot_name=yaskawa
+planning_group_name=gp4_toolchange
+ikfast_plugin_pkg=gp4_toolchange_ikfast_plugin
+base_link_name=yaskawa_base_link
+eef_link_name=axia80_mate
+ikfast_output_path=/home/rob/checkout/tmp_ikfast_workspace/gp4_toolchange_ikfast_plugin/src/yaskawa_gp4_toolchange_ikfast_solver.cpp
+
+rosrun moveit_kinematics create_ikfast_moveit_plugin.py\
+  --search_mode=$search_mode\
+  --srdf_filename=$srdf_filename\
+  --robot_name_in_srdf=$robot_name_in_srdf\
+  --moveit_config_pkg=$moveit_config_pkg\
+  $robot_name\
+  $planning_group_name\
+  $ikfast_plugin_pkg\
+  $base_link_name\
+  $eef_link_name\
+  $ikfast_output_path
